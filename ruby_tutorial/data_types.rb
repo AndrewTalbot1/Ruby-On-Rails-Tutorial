60703888bed1f2e7852a4820f4394f9e1e834d90
Andrew Talbot <andrewtalbotprogramming@gmail.com>
2020-08-12 11:26:13 -0500

    quick over view of ruby datatypes

---
+++ ruby_tutorial/data_types.rb
@@ -0,0 +1,17 @@
+# String
+name = "Andrew"
+occupation = "programmer"
+
+#Integers
+age = 20
+
+#Floating point numbers
+pie = 3.14
+
+
+#Boolean fields
+istall = true
+isperson = true
+
+#No data types = nil
+language = nil
