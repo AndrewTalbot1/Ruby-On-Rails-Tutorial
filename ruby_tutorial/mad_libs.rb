11e671429c4114ca84653df20592b262b0f49788
Andrew Talbot <andrewtalbotprogramming@gmail.com>
2020-08-12 13:59:41 -0500

    Simple madlibs game

---
+++ ruby_tutorial/mad_libs.rb
@@ -0,0 +1,11 @@
+# Here is a madlibs game
+puts("Please enter a color: ")
+color = gets.chomp()
+puts("Please enter a plural noun: ")
+plural_noun = gets.chomp()
+puts("Please enter a name: ")
+name = gets.chomp()
+
+puts("Roses are " + color)
+puts(plural_noun + " are blue")
+puts("I love you " + name)
