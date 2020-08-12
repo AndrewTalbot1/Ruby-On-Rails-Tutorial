d7a4f3e2425e16e4aef42e2fcdb5e2a5f7fabab7
Andrew Talbot <andrewtalbotprogramming@gmail.com>
2020-08-12 12:10:46 -0500

    Quick overview of numbers with ruby

---
+++ ruby_tutorial/numbers.rb
@@ -0,0 +1,26 @@
+#using numbers in ruby
+num = 20
+puts ("Here is a number and the number is " + num.to_s)
+
+num2 = -20
+
+puts num2.abs()
+
+round_numbers = 1.2345
+puts round_numbers.round()
+
+ceiling = 20.1
+puts ceiling.ceil()
+
+floor_value = 7.1
+puts floor_value.floor()
+
+operations = 9
+puts Math.sqrt(operations)
+
+
+# Any operation with integers will return an integer
+puts 1 + 7
+
+# Any operation with at least one float number the result will be a float
+puts 1.0 + 7
